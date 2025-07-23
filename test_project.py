4b9a38ab6c4a2b358d9f31b20c4d9a8437848f05
alirezatd80 <alirezatd80@gmail.com>
2025-07-23 20:52:09 +0330

    add test for check_number

--- test_project.py
+++ test_project.py
@@ -14,4 +14,8 @@ def test_litter():
     assert password_checker.check_litter('asdasdmSssa') == True
     assert password_checker.check_litter('12314353453') == False
     
+def test_digit():
+    assert password_checker.check_number('sxqwe1213')==True
+    assert password_checker.check_number('asfadsfadsf') == False
+    assert password_checker.check_number('123123123') == True
     
