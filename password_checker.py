60ee5a96f923203661305139c14e14bb85f9dba8
alirezatd80 <alirezatd80@gmail.com>
2025-07-23 20:39:03 +0330

    check litter

--- password_checker.py
+++ password_checker.py
@@ -3,3 +3,15 @@ def check_length(password):
     if length < 7 :
         raise Exception('your password is short')
     return True
+
+
+def check_litter(password):
+    upper_litter = any(char.isupper() for char in password)
+    lower_litter = any(char.lower() for char in password)
+
+    if upper_litter and lower_litter :
+        return True
+    return False
+
+print(check_litter('12314353453'))
+
